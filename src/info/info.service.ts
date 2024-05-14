59abb8dc61d26d4e9a23580345afce62df239a81
Riccardo Lin <chenweiriccardo.lin@gmail.com>
2024-05-14 22:14:34 +0200

    feat(user validate): add validation of form

--- src/info/info.service.ts
+++ src/info/info.service.ts
@@ -3,7 +3,7 @@ import { plainToClass } from 'class-transformer';
 import { validate } from 'class-validator';
 import { UpdateInfoRequest as UpdateInfoRequestInterface } from './interfaces';
 import { BaseResponse } from '../interfaces';
-import { UpdateInfoRequest } from './models';
+import { UpdateInfoRequest, UpdateUserInfoRequest } from './models';
 
 @Injectable()
 export class InfoService {
@@ -12,6 +12,24 @@ export class InfoService {
   ): Promise<BaseResponse> {
     const data = plainToClass(UpdateInfoRequest, rawData);
     const validationErrors = await validate(data);
+    if (validationErrors.length > 0) {
+      return {
+        success: false,
+        errors: validationErrors,
+      };
+    }
+    return {
+      success: true,
+      data,
+    };
+  }
+
+  async validateUserInfo(
+    rawData: UpdateInfoRequestInterface,
+  ): Promise<BaseResponse> {
+    const data = plainToClass(UpdateUserInfoRequest, rawData);
+    const validationErrors = await validate(data);
+
     if (validationErrors.length > 0) {
       return {
         success: false,
--- src/info/interfaces/index.ts
+++ src/info/interfaces/index.ts
@@ -1,3 +1,10 @@
 export interface UpdateInfoRequest {
   name: string;
 }
+
+export interface UpdateUserInfoRequest {
+  name: string;
+  age: number;
+  married?: boolean;
+  birthdate: string;
+}
